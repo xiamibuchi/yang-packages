cfc7b853f82bc8b6698429e50946e5474e9ff189
shenyang <shenyang@xd.com>
2024-03-18 21:48:38 +0800

    fix: ts

--- packages/utils/src/objects.ts
+++ packages/utils/src/objects.ts
@@ -3,6 +3,9 @@ import type { Arrayable } from '.';
 
 export { hasOwn } from '@vue/shared';
 
+export const keysOf = <T extends object>(obj: T) =>
+  Object.keys(obj) as Array<keyof T>;
+
 export const getProp = <T = any>(
   obj: Record<string, any>,
   path: Arrayable<string>,
