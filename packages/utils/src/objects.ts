8de4718e89cb5b602b912298e3fbe92ad595da9b
shenyang <shenyang@xd.com>
2024-03-19 11:48:33 +0800

    chore: remove code

--- packages/utils/src/objects.ts
+++ packages/utils/src/objects.ts
@@ -1,22 +1,4 @@
-import { get, set } from 'lodash-es';
-import type { Arrayable } from '.';
-
 export { hasOwn } from '@vue/shared';
 
 export const keysOf = <T extends object>(obj: T) =>
   Object.keys(obj) as Array<keyof T>;
-
-export const getProp = <T = any>(
-  obj: Record<string, any>,
-  path: Arrayable<string>,
-  defaultValue?: any
-): { value: T } => {
-  return {
-    get value() {
-      return get(obj, path, defaultValue);
-    },
-    set value(val: any) {
-      set(obj, path, val);
-    },
-  };
-};
