1c1e0083c0e8897084ff57690591c9cd8bf43d88
shenyang <shenyang@xd.com>
2024-03-21 17:05:31 +0800

    chore: eslint 配置从 eslint-config package 迁移到 root

---
+++ .eslintrc.js
@@ -0,0 +1,219 @@
+const DEFAULT_EXTENDS = [
+  'plugin:markdown/recommended-legacy',
+  'eslint:recommended',
+  'plugin:import/recommended',
+  'plugin:jsonc/recommended-with-jsonc',
+  'plugin:@typescript-eslint/recommended',
+  'prettier',
+];
+
+const VUE_EXTENDS = [...DEFAULT_EXTENDS, 'plugin:vue/vue3-recommended'];
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: DEFAULT_EXTENDS,
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.mjs', '.ts', '.d.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'script',
+  },
+  rules: {
+    'import/extensions': ['error', { js: 'always' }],
+    camelcase: ['error', { properties: 'never' }],
+    'no-console': ['warn', { allow: ['error'] }],
+    'no-debugger': 'warn',
+    'no-constant-condition': ['error', { checkLoops: false }],
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    'no-return-await': 'error',
+    'no-var': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'prefer-const': [
+      'warn',
+      { destructuring: 'all', ignoreReadBeforeAssign: true },
+    ],
+    'prefer-arrow-callback': [
+      'error',
+      { allowNamedFunctions: false, allowUnboundThis: true },
+    ],
+    'object-shorthand': [
+      'error',
+      'always',
+      { ignoreConstructors: false, avoidQuotes: true },
+    ],
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'error',
+
+    // best-practice
+    'array-callback-return': 'error',
+    'block-scoped-var': 'error',
+    'no-alert': 'warn',
+    'no-case-declarations': 'error',
+    'no-multi-str': 'error',
+    'no-with': 'error',
+    'no-void': 'error',
+
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false,
+      },
+    ],
+
+    // stylistic-issues
+    'prefer-exponentiation-operator': 'error',
+
+    // ts
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { disallowTypeAnnotations: false },
+    ],
+    '@typescript-eslint/ban-ts-comment': ['off', { 'ts-ignore': false }],
+
+    // vue
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-explicit-emits': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/prefer-import-from-vue': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+
+    // prettier
+    'prettier/prettier': 'error',
+
+    // import
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+
+        pathGroups: [
+          {
+            pattern: 'vue',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@vue/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@syseven/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['type'],
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/namespace': 'off',
+    'import/default': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/named': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.json', '*.json5', '*.jsonc'],
+      parser: 'jsonc-eslint-parser',
+    },
+    {
+      files: ['*.ts', '*.tsx', '*.vue'],
+      rules: {
+        // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
+        // does not work with type definitions
+        'no-unused-vars': 'off',
+        // TS already checks for that, and Typescript-Eslint recommends to disable it
+        // https://typescript-eslint.io/linting/troubleshooting#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+      },
+    },
+    {
+      files: ['*.d.ts'],
+      rules: {
+        'import/no-duplicates': 'off',
+      },
+    },
+    {
+      files: ['apps/docs', 'apps/nuxt3-demo'],
+      extends: VUE_EXTENDS,
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue'],
+        ecmaVersion: 'latest',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      rules: {
+        'no-undef': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+      },
+    },
+    {
+      files: ['**/*.md/*.js', '**/*.md/*.ts', '**/*.md/*.vue'],
+      rules: {
+        'no-console': 'off',
+        'import/no-unresolved': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'no-unused-vars': 'off',
+        'no-alert': 'off',
+        'no-debugger': 'off',
+        'getter-return': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-redeclare': 'off',
+        '@typescript-eslint/no-this-alias': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+      },
+    },
+  ],
+};
