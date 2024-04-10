f447ed711d639a9e6cab7b94b29cab42a33309de
shenyang <shenyang@xd.com>
2024-04-10 17:25:49 +0800

    feat: update extension

---
+++ apps/minify-font/index.js
@@ -0,0 +1,17 @@
+import Fontmin from 'fontmin';
+import fs from 'fs-extra';
+
+const text = fs.readFileSync('langs/chinese.txt', 'utf8');
+const fontmin = new Fontmin()
+  .use(Fontmin.glyph({ text }))
+  .use(Fontmin.ttf2woff2())
+  .src('fonts/*.ttf')
+  .dest('dist');
+
+fontmin.run((err, files) => {
+  if (err) {
+    throw err;
+  }
+  console.log(files[0]);
+  // => { contents: <Buffer 00 01 00 ...> }
+});
