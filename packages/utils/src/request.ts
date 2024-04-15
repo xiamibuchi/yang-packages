17feb44f26689194fb249c0ed0db2695a033dae8
shenyang <shenyang@xd.com>
2024-04-15 20:10:13 +0800

    feat: 队列请求

--- packages/utils/src/request.ts
+++ packages/utils/src/request.ts
@@ -1,4 +1,5 @@
 import { isServer } from './env';
+import type { UnknowFunction } from './typescript';
 
 export const toFormData = (data: any) => {
   const formData = isServer() ? new URLSearchParams() : new FormData();
@@ -23,4 +24,38 @@ export function getXff(xff: string, remoteAddress?: string) {
   const xffList = xff.split(',').map((x) => x.trim());
   xffList.push(remoteAddress);
   return xffList.join(', ');
+}
+
+export class RequestQueue {
+  limit: number;
+  current: number;
+  queue: UnknowFunction[];
+  constructor(config?: { limit?: number }) {
+    this.limit = config?.limit || 6;
+    this.current = 0;
+    this.queue = [];
+  }
+  add(fn: UnknowFunction) {
+    this.queue.push(fn);
+    this.next();
+  }
+  // 内部方法
+  async _run(fn: UnknowFunction, callback: () => void) {
+    try {
+      await fn();
+    } catch (e) {
+      console.error(e);
+    }
+    callback();
+  }
+  next() {
+    while (this.current < this.queue.length && this.queue.length > 0) {
+      const fn = this.queue.shift()!;
+      this.current++;
+      this._run(fn, () => {
+        this.current--;
+        this.next();
+      });
+    }
+  }
 }
