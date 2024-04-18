b89aaf23bb9604a58ec86be1859fd37f39cd3ed9
shenyang <shenyang@xd.com>
2024-04-18 20:41:36 +0800

    feat: react demo

---
+++ apps/next-demo/src/app/test/components/StopWatch/StopWatch.tsx
@@ -0,0 +1,96 @@
+import {
+  type ForwardedRef,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+const RefComponent = forwardRef(
+  (props, ref: ForwardedRef<HTMLInputElement> | undefined) => {
+    return <input {...props} ref={ref} />;
+  },
+);
+function VideoPlayer({ src, isPlaying }: { src: string; isPlaying: boolean }) {
+  const ref = useRef<HTMLVideoElement | null>(null);
+  useEffect(() => {
+    if (isPlaying) {
+      ref.current?.play();
+    } else {
+      ref.current?.pause();
+    }
+    // Cleanup
+    return () => {
+      ref.current?.pause();
+    };
+  }, [isPlaying]);
+
+  return <video ref={ref} src={src} loop playsInline />;
+}
+
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState<number | null>(null);
+  const timer = useRef<number | null>(null);
+  const domRef = useRef(null);
+
+  function handleStart() {
+    if (!startTime) {
+      setStartTime(Date.now());
+    }
+    setNow(Date.now());
+
+    timer.current = window.setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    timer.current && window.clearInterval(timer.current);
+  }
+
+  function handleReset() {
+    handleStop();
+    setStartTime(null);
+    setNow(null);
+  }
+
+  function handleRef() {
+    console.log(domRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  function handleInputClick() {
+    inputRef.current?.focus();
+  }
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <>
+      <h1 ref={domRef}>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <br />
+      <button onClick={handleStop}>Stop</button>
+      <br />
+      <button onClick={handleReset}>Reset</button>
+      <br />
+      <button onClick={handleRef}>Ref</button>
+      <br />
+      <RefComponent ref={inputRef} />
+      <button onClick={handleInputClick}>Focus the input</button>
+      <div>
+        <button onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? 'Pause' : 'Play'}
+        </button>
+        <VideoPlayer
+          isPlaying={isPlaying}
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+        />
+      </div>
+    </>
+  );
+}
--- apps/next-demo/src/app/test/page.tsx
+++ apps/next-demo/src/app/test/page.tsx
@@ -1,22 +1,76 @@
 'use client';
-import { useState } from 'react';
+import { useReducer, useRef, useState } from 'react';
+import { useOnlineStatus } from '../../hooks/use-online-status';
 import Profile from './profile';
 import './styles.scss';
+import StopWatch from './components/StopWatch/StopWatch.tsx';
+
+const initialState = { count: 0 };
+
+function reducer(
+  state: {
+    count: number;
+  },
+  action: {
+    type: string;
+  },
+) {
+  switch (action.type) {
+    case 'increment':
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case 'decrement':
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      throw new Error();
+  }
+}
+
+function StatusBar() {
+  const isOnline = useOnlineStatus();
+  return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+}
 
 export default function TestPage() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const ref = useRef(0); // { current: 0 } // 修改 state 不会触发重新渲染
   function MyButton() {
     const [count, setCount] = useState(0);
     function handleClick() {
+      ref.current = ref.current + 1;
       setCount(count + 1);
+      alert(`You clicked ${ref.current} times!`);
     }
     return <button onClick={handleClick}>I'm {count}</button>;
   }
   return (
     <div className="test-page">
+      <StatusBar />
       <Profile />
       <MyButton />
       <br></br>
       <MyButton />
+      <div>
+        <div>Count: {state.count}</div>
+        <button
+          className="font-bold"
+          onClick={() => dispatch({ type: 'increment' })}
+        >
+          +
+        </button>
+        <button
+          className="font-bold"
+          onClick={() => dispatch({ type: 'decrement' })}
+        >
+          -
+        </button>
+      </div>
+      <StopWatch />
     </div>
   );
 }
