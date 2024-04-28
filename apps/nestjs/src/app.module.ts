935e400de95f66a7584ef75c0c1c57a081bcfc32
shenyang <shenyang@xd.com>
2024-04-28 18:18:46 +0800

    feat: middleware

--- apps/nestjs/src/app.module.ts
+++ apps/nestjs/src/app.module.ts
@@ -1,5 +1,11 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { AppService } from './app.service';
 import { HttpStaticModule } from './http-static/http-static.module';
 
@@ -8,4 +14,10 @@ import { HttpStaticModule } from './http-static/http-static.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '/api/*', method: RequestMethod.GET });
+  }
+}
